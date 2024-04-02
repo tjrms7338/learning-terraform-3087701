89bf0cd159e4bf0558db4b00f345a1cd3983239c
Terrence Morris <terrence.morris@tradingtechnologies.com>
2024-04-02 10:15:27 -0500

    Update main.tf update ami

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "app_ami" {
   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
+#  filter {
+#    name   = "name"
+#    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+#  }
 
   filter {
     name   = "virtualization-type"
@@ -15,7 +15,7 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
+  ami                    = "ami-02cafb524b3926df9"
   instance_type          = var.instance_type
   vpc_security_group_ids = ["sg-0bb56c6c"]
   tags = {
